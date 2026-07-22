5f3372172d4329866ddd320797d56a6438e1b1e4
Logan Brough <154569848+loganbrough@users.noreply.github.com>
2026-07-22 16:52:47 -0700

    Add: string-encode-and-decode - submission-19

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-19.py
@@ -0,0 +1,26 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        s = []
+        for string in range(len(strs)):
+            if len(strs) > 0:
+                s = ".".join(strs)
+        return str(s)
+
+    def decode(self, s: str) -> List[str]:
+        spacenum = 0
+        element = 0
+        if s == str([]):
+            return []
+        else:
+            for l in s:
+                if l == ".":
+                    spacenum += 1
+            decoded_s = ["" for i in range(spacenum+1)]
+
+            for l in s: 
+                if l == ".":
+                    element += 1
+                    continue
+                decoded_s[element] += l
+            
+            return decoded_s
