79a8e117b2eff4d745f9517c35ca18e6475d8726
Logan Brough <154569848+loganbrough@users.noreply.github.com>
2026-07-22 16:51:15 -0700

    Add: string-encode-and-decode - submission-13

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-13.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+#{"key":"value", "key":"value"}
+    def encode(self, strs: List[str]) -> str:
+        s = []
+        str_dict = defaultdict(list)
+        for string in range(len(strs)): #O(m) where m is len(strs)
+            str_dict[string] = strs[string]
+            if len(strs) > 0:
+                s = ".".join(str_dict.values())
+                #s = ".".join(str_dict.values())
+        return str(s)
+
+    def decode(self, s: str) -> List[str]:
+        spacenum = 0
+        element = 0
+        if s == str([]):
+            return []
+        else:
+            for l in s: #O(m)
+                if l == ".":
+                    spacenum += 1
+            decoded_s = ["" for i in range(spacenum+1)]
+
+            for l in s: 
+                if l == ".":
+                    element += 1
+                    continue
+                decoded_s[element] += l
+            
+            return decoded_s
